14665ed9d1bf6c9ceb5c5ccadc6a4709cb71dcde
Barbara Jeziorek <barbarajeziorek@gmail.com>
2026-01-01 20:24:32 +0100

    daisyui4 + tailwind3

---
+++ selfscape/src/components/MapNode.tsx
@@ -0,0 +1,13 @@
+//import { useState } from "react"
+
+function MapNode(){
+   // const[count, setCount] = useState(0)
+
+    return(
+        <div>
+            <p>karta</p>
+        </div>
+    )
+}
+
+export default MapNode
